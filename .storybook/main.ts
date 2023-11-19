262ae16fdd9290c3d799d6bb611f8b8d8e720d27
hrivera88 <hrivera@outlook.com>
2023-11-18 21:18:55 -0600

    Created ESLint, Tailwind, Prettier, Next, Storybook, PostCSS and new packages like SVGR

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,64 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs';
+import { RuleSetRule } from 'webpack';
+
+const path = require('path');
 
 const config: StorybookConfig = {
-  stories: [
-    "../components/**/*.stories.@(tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: ['../components/**/*.stories.@(tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-interactions',
+		'@storybook/addon-styling-webpack',
+		{
+			name: '@storybook/addon-styling-webpack',
+
+			options: {
+				rules: [
+					{
+						test: /\.css$/,
+						sideEffects: true,
+						use: [
+							require.resolve('style-loader'),
+							{
+								loader: require.resolve('css-loader'),
+								options: {
+									importLoaders: 1,
+								},
+							},
+							{
+								loader: require.resolve('postcss-loader'),
+								options: {
+									implementation: require.resolve('postcss'),
+								},
+							},
+						],
+					},
+				],
+			},
+		},
+	],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
+	webpackFinal: (config) => {
+		const rules = config?.module?.rules as RuleSetRule[];
+		const imageRule = rules.find(
+			(rule) => rule?.test instanceof RegExp && rule.test.test('.svg')
+		);
+		if (imageRule) {
+			imageRule.exclude = /\.svg$/;
+		}
+		rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
 };
 export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'navy': '#0e4f74',
-        'grey': '#48484A',
-        'light-blue': '#a5dbdb',
-        'yellow': '#ffd889',
-        'peach': '#f9ceb3',
-        'light-grey': '#9e9d9e',
-        'teal': '#278b93',
-        'pink': '#f8b4bd',
-        'light-green': '#bfe0bb',
-        'purple': '#955683',
-        'green': '#7cb65e'
-      }
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				navy: '#0e4f74',
+				grey: '#48484A',
+				'light-blue': '#a5dbdb',
+				yellow: '#ffd889',
+				peach: '#f9ceb3',
+				'light-grey': '#9e9d9e',
+				teal: '#278b93',
+				pink: '#f8b4bd',
+				'light-green': '#bfe0bb',
+				purple: '#955683',
+				green: '#7cb65e',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
