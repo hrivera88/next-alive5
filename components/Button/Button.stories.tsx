e768f83dc0d30f1f4ba3edbb733a8aa0b28b1038
hrivera88 <hrivera@outlook.com>
2023-11-18 21:20:26 -0600

    Created Button component and stories

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,18 +1,95 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Button from './Button';
+import ChatBubbleIcon from '../../public/icons/ChatBubbleIcon.svg';
+import { BarLoader } from 'react-spinners';
 
 const meta: Meta<typeof Button> = {
-    component: Button,
-    title: 'Components/Button'
-}
+	component: Button,
+	title: 'Components/Button',
+};
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-
 export const Primary: Story = {
-    args: {
-        text: 'Login',
-        className: 'py-2 px-4 bg-navy text-white rounded border-0'
-    }
+	args: {
+		text: 'Login',
+		className:
+			'py-2 px-4 bg-navy text-white text-base hover:bg-navy/75 rounded border-0',
+	},
+};
+
+export const Disabled: Story = {
+	args: {
+		text: 'Login',
+		className:
+			'py-2 px-4 bg-navy/50 text-white/50 text-base cursor-not-allowed rounded border-0',
+	},
+};
+
+export const Secondary: Story = {
+	args: {
+		text: 'Cancel',
+		className:
+			'py-2 px-4 bg-transparent text-navy text-base hover:underline rounded border-0',
+	},
+};
+
+export const Small: Story = {
+	args: {
+		text: 'Login',
+		className:
+			'py-1 px-2 bg-navy text-white text-sm hover:bg-navy/75 rounded border-0',
+	},
+};
+
+export const PrimaryIcon: Story = {
+	args: {
+		text: 'Live Chat',
+		className:
+			'py-2 px-4 bg-navy text-white text-base hover:bg-navy/75 flex flex-row items-center rounded border-0',
+		icon: <ChatBubbleIcon className="w-4 h-4 mr-2 fill-current" />,
+	},
+};
+
+export const Icon: Story = {
+	args: {
+		className:
+			'py-2 px-2 bg-navy text-white text-base flex flex-row hover:bg-navy/75 items-center rounded border-0',
+		icon: <ChatBubbleIcon className="w-4 h-4 fill-current" />,
+	},
+};
+
+export const SecondaryIcon: Story = {
+	args: {
+		text: 'Lets Chat',
+		className:
+			'py-2 px-4 bg-transparent text-navy flex flex-row items-center text-base hover:underline rounded border-0',
+		icon: <ChatBubbleIcon className="w-4 h-4 mr-2 fill-current" />,
+	},
+};
+
+export const Danger: Story = {
+	args: {
+		text: 'Delete',
+		className:
+			'py-2 px-4 bg-red-600 text-white text-base hover:bg-red-600/75 rounded border-0',
+	},
+};
+
+export const Success: Story = {
+	args: {
+		text: 'Online',
+		className:
+			'py-2 px-4 bg-green text-white text-base hover:bg-green/75 rounded border-0',
+	},
+};
+
+export const Loading: Story = {
+	args: {
+		loader: <BarLoader color="#fff" width={100} height={4} loading={true} />,
+		isLoading: true,
+		className:
+			'py-4 px-4 bg-navy text-white text-base hover:bg-navy/75 rounded border-0',
+	},
 };
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,24 +2,47 @@ import React from 'react';
 
 // Define Button Props
 
-interface ButtonProps { 
-    text?: string;
-    icon?: React.ReactNode;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    style?: React.CSSProperties;
-    className?: string;
+interface ButtonProps {
+	text?: string;
+	icon?: React.ReactNode;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	style?: React.CSSProperties;
+	className?: string;
+	isLoading?: boolean;
+	loader?: React.ReactNode;
+	type?: 'button' | 'submit' | 'reset';
+	disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, icon, onClick, style, className, ...props }) => {
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onClick?.(event);
-     }
-    return (
-        <button onClick={handleClick} style={style} className={`${className ?? ''}`} {...props}>
-            {icon && <span className='button-icon'>{icon}</span>}
-            {text && <span className='button-text'>{text}</span> }
-        </button>
-    );
-}
- 
+const Button: React.FC<ButtonProps> = ({
+	text,
+	icon,
+	onClick,
+	style,
+	className,
+	isLoading,
+	loader,
+	disabled,
+	type = 'button',
+	...props
+}) => {
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		onClick?.(event);
+	};
+	return (
+		<button
+			onClick={handleClick}
+			style={style}
+			disabled={disabled}
+			type={type}
+			className={`${className ?? ''}`}
+			{...props}
+		>
+			{!isLoading && icon && <span className="button-icon">{icon}</span>}
+			{!isLoading && text && <span className="button-text">{text}</span>}
+			{isLoading && <span className="button-loader block">{loader}</span>}
+		</button>
+	);
+};
+
 export default Button;
